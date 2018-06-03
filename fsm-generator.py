a9050dc4217b8ff19e47b903c26f70624cab83cf
Titan <howay.tan@gmail.com>
2018-06-03 17:04:50 +0800

    Add table driven style to generated codes

--- fsm-generator.py
+++ fsm-generator.py
@@ -1,8 +1,8 @@
 #! /usr/bin/python3
 
 def normalize(string):
-    if len(string) > 0 and string[0].isdigit():
-        string = "number_" + string
+    #if len(string) > 0 and string[0].isdigit():
+    #    string = "number_" + string
     return string.replace(' == ', '_EQUALS_').replace(' != ', '_NOT_EQUALS_').replace(':=', '_ASSIGN_TO_').replace(' = ', '_EQUALS_').replace('=', '_EQUALS_').replace(' + ', '_PLUS_').replace('+', '_PLUS_').replace(' - ', '_MINUS_').replace('-', '_').replace(' > ', '_GREATER_THAN_').replace('>', 'GREATER_THAN').replace(' < ', '_LESS_THAN_').replace('<', 'LESS_THAN').replace(': ', '_COLON_').replace(':', '_COLON_').replace(', ', '_COMMA_').replace(',', '_COMMA_').replace('"', '_QUOTATION_').replace('.', '_DOT_').replace('?', '_QUESTION_').replace(' ', '_').replace('\n', '_NEWLINE_').replace('#', 'SHARP').upper()
 
 def load_model(prefix, filename):
@@ -38,27 +38,33 @@ def load_model(prefix, filename):
     return states, events, actions.keys(), transformings
 
 def state(prefix, states):
+    i = 0
     output = "enum %s_STATE {\n" % prefix.upper()
     for s in states:
-        output += " " * 2 + s + ",\n"
+        output += " " * 2 + s + " = %d,\n" % i
+        i += 1
     output += "};\n"
     return output
 
 def event(prefix, events):
+    i = 0
     output = "enum %s_EVENT {\n" % prefix.upper()
     for e in events:
-        output += " " * 2 + e + ",\n"
+        output += " " * 2 + e + " = %d,\n" % i
+        i += 1
     output += "};\n"
     return output
 
 def action(prefix, actions):
+    i = 1
     output = "enum %s_ACTION {\n" % prefix.upper()
     for a in actions:
-        output += " " * 2 + a + ",\n"
+        output += " " * 2 + a + " = %d,\n" % i
+        i += 1
     output += "};\n"
     return output
 
-def transforming(prefix, states, events, transformings, debug):
+def code_transforming(prefix, states, events, transformings, debug):
     output = "enum %s_STATE %s_transform_state(enum %s_STATE state, enum %s_EVENT event, void * data) {\n" % (prefix, prefix.lower(), prefix, prefix)
     output += ' ' * 2 + 'switch (event) {\n'
     for i in range(len(events)):
@@ -91,7 +97,56 @@ def transforming(prefix, states, events, transformings, debug):
     output += "}\n"
     return output
 
-def main(src, prefix, directory, defination, implementation, debug):
+def table_transforming(prefix, states, events, actions, transformings, debug):
+    # calculate action type
+    if len(actions) > 256 and len(actions) <= 65536:
+        action_type = 'unsigned short'
+    elif len(actions) > 65536:
+        action_type = 'unsigned int'
+    else:
+        action_type = 'unsigned char'
+
+    # calculate state type
+    if len(states) > 256 and len(states) <= 65536:
+        state_type = 'unsigned short'
+    elif len(actions) > 65535:
+        state_type = 'unsigned int'
+    else:
+        state_type = 'unsigned char'
+
+    output = state_type + " %s_transform_states[%d][%d] = {\n" % (prefix.lower(), len(states), len(events))
+    for i in range(len(states)):
+        output += ' ' * 2 + '{'
+        for j in range(len(events)):
+            (action, state) = transformings[i][j]
+            if state:
+                output += ' ' + str(state) + ','
+            else:
+                output += ' ' + str(states[i]) + ','
+        output = output[0:-1]
+        output += ' ' + '},\n'
+    output += "};\n"
+    output += action_type + " %s_transform_actions[%d][%d] = {\n" % (prefix.lower(), len(states), len(events))
+    for i in range(len(states)):
+        output += ' ' * 2 + '{'
+        for j in range(len(events)):
+            (action, state) = transformings[i][j]
+            if action:
+                output += ' ' + str(action) + ','
+            else:
+                output += ' 0,'
+        output = output[0:-1]
+        output += ' ' + '},\n'
+    output += "};\n"
+    output += "enum %s_STATE %s_transform_state(enum %s_STATE state, enum %s_EVENT event, void * data) {\n" % (prefix, prefix.lower(), prefix, prefix)
+    if debug:
+        output += ' ' * 2 + 'printf("%s_EVENT %%d occurred in state %%d\\n", event, state);\n' % (prefix)
+    output += ' ' * 2 + '%s_do_action(%s_transform_actions[state][event], data);\n' % (prefix.lower(), prefix.lower())
+    output += ' ' * 2 + 'return %s_transform_states[state][event];\n' % (prefix.lower())
+    output += "}\n"
+    return output
+
+def main(src, prefix, directory, defination, implementation, debug, style):
     (states, events, actions, transformings) = load_model(prefix, src)
     import os.path
     if directory == None:
@@ -115,7 +170,10 @@ def main(src, prefix, directory, defination, implementation, debug):
         if debug:
             output.write('#include <stdio.h>\n')
         output.write('#include "%s"\n' % header)
-        output.write(transforming(prefix, states, events, transformings, debug))
+        if style == "code":
+            output.write(code_transforming(prefix, states, events, transformings, debug))
+        else:
+            output.write(table_transforming(prefix, states, events, actions, transformings, debug))
         output.write("\n");
 
 if __name__ == '__main__':
@@ -128,5 +186,6 @@ if __name__ == '__main__':
     parser.add_argument("--defination", help="The filename of definations header")
     parser.add_argument("--implementation", help="The filename of implementation")
     parser.add_argument("--debug", action='store_true', help="Output debug info in console")
+    parser.add_argument("--style", default="code", help="The style of fsm: code(code directly) or table(table driven)")
     args = parser.parse_args()
-    main(args.src, normalize(args.prefix), args.directory, args.defination, args.implementation, args.debug)
+    main(args.src, normalize(args.prefix), args.directory, args.defination, args.implementation, args.debug, args.style)
